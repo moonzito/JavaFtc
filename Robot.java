170f43fa518f292ce175d3f5ec1b510cac70a6f2
Carlos Gabriel <moon_ita_mit_04@hotmail.com>
2024-09-12 09:50:42 -0300

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Tutorial;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Robot{
+
+  public HardwareMap hardwareMap;
+  
+  public DcMotor leftFront;
+  public DcMotor rightFront;
+  public DcMotor leftBack;
+  public DcMotor rightBack:
+
+  public Servo claw;
+
+  public Robot(){
+      leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+      RightFront = hardwareMap.get(DcMotor.class, "RightFront");
+      leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+      RightBack = hardwareMap.get(DcMotor.class, "RightBack");
+  }
+  
+}
